67a10ce60cfd90812df676eddc07adcd32b48300
Ryota Asai <ryota846538@outlook.jp>
2023-11-28 12:19:17 +0900

    update...

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const coronaData = require("./coronaData");
+
+app.get("/", function (req, res) {
+  res.send(coronaData);
+});
 
 app.listen(5000, function () {
   console.log("Server is running on port 5000");
